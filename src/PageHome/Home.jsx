1a37cbe2f60da6c16b267b922d37a4ec5479900e
NetaL <tushal3@gmail.com>
2018-01-21 17:08:36 +0200

    sidebar pusher responsive width feature added

--- src/PageHome/Home.jsx
+++ src/PageHome/Home.jsx
@@ -21,19 +21,13 @@ class Home extends Component {
     return (
       <SidebarMenu {...sidebarMenuProps}>
         <HeaderMain {...headerMainProps}/>
+        {/* TODO: container fix: always position children to center */}
         <Container text style={{ marginTop: '7em' }}>
           <h2>Home</h2>
           <Header as='h1'>Semantic UI React Fixed Template</Header>
           <p>This is a basic fixed menu template using fixed size containers.</p>
           <p>A text container is used for the main container, which is useful for single column layouts.</p>
 
-          <Image src='/assets/images/wireframe/media-paragraph.png' style={{ marginTop: '2em' }} />
-          <Image src='/assets/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-          <Image src='/assets/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-          <Image src='/assets/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-          <Image src='/assets/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-          <Image src='/assets/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-          <Image src='/assets/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
         </Container>
 
       </SidebarMenu>
--- src/components/SidebarMenu/SidebarMenu.jsx
+++ src/components/SidebarMenu/SidebarMenu.jsx
@@ -2,31 +2,33 @@ import React, { Component } from 'react'
 import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react'
 
 class SidebarMenu extends Component {
-  
+
   render() {
     const { sidebarVisible } = this.props;
+    const pusherStyle = sidebarVisible === true ? { width: 'calc(100% - 350px)' } : {};
     return (
-        <Sidebar.Pushable as={Segment}>
-          <Sidebar as={Menu} animation='push' width='thin' visible={sidebarVisible} icon='labeled' vertical inverted>
-            <Menu.Item name='home'>
-              <Icon name='home' />
-              Home
+      <Sidebar.Pushable as={Segment}>
+        <Sidebar as={Menu} width='wide' visible={sidebarVisible} icon='labeled' vertical inverted>{/*animation='push' */}
+          <Menu.Item name='home'>
+            <Icon name='home' />
+            Home
             </Menu.Item>
-            <Menu.Item name='gamepad'>
-              <Icon name='gamepad' />
-              Games
+          <Menu.Item name='gamepad'>
+            <Icon name='gamepad' />
+            Games
             </Menu.Item>
-            <Menu.Item name='camera'>
-              <Icon name='camera' />
-              Channels
+          <Menu.Item name='camera'>
+            <Icon name='camera' />
+            Channels
             </Menu.Item>
-          </Sidebar>
-          <Sidebar.Pusher>
-            <Segment basic>
-              {this.props.children}
-            </Segment>
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
+        </Sidebar>
+        <Sidebar.Pusher style={pusherStyle}>
+          {/* TODO: refactor sidebar fix to make it steady, modifiable and reusable using a property to handle the animation override & new responsive pusher width feature */}
+          {/* <Segment basic> */}
+          {this.props.children}
+          {/* </Segment> */}
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>
     )
   }
 }
